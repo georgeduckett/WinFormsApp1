1a2faf949f67af287f50833071e6ddb8430eda72
George Duckett <george.duckett@aqualisa.co.uk>
2023-06-21 09:20:52 +0100

    Remove use of a timer and use a fixed time game loop

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,24 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
-            // 
-            // gameTimer
-            // 
-            this.gameTimer.Enabled = true;
-            this.gameTimer.Interval = 50;
-            this.gameTimer.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
 
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -6,16 +8,65 @@ namespace WinFormsApp1
         private int PlayerSpeedY = 5;
         public Drawable Enemy = new Drawable(50, 123, 20, 20, Color.Black);
         public Drawable Player = new Drawable(150, 300, 20, 20, Color.Blue);
+
+        // fields for controlling the game timer
+        private Stopwatch stopWatch = Stopwatch.StartNew();
+        private readonly TimeSpan TargetElapsedTime = TimeSpan.FromTicks(TimeSpan.TicksPerSecond / 60); // 60 fps
+        private readonly TimeSpan MaxElapsedTime = TimeSpan.FromTicks(TimeSpan.TicksPerSecond / 10); // The max number of updates in a row it'll do to 'catch up'
+        private TimeSpan accumulatedTime; // How much time has passed
+        private TimeSpan lastTime; // The last time we ran an update
+
         public Form1()
         {
             InitializeComponent();
+            Application.Idle += Application_Idle; // When the application is idle the system will call the Application_Idle method
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void Application_Idle(object? sender, EventArgs e)
+        {
+            // Application has done everything it needs to, so we can run our game
+            while (!NativeMethods.PeekMessage(out _, IntPtr.Zero, 0, 0, 0))
+            { // While there are no 'events' (keyboard/mouse etc) that the form needs to handle we run our game in a loop
+                Tick();
+            }
+        }
+
+        private void Tick()
+        {
+            var currentTime = stopWatch.Elapsed; // We save this to a variable as we use it twice, and it could change inbetween
+
+            var elapsedTime = currentTime - lastTime; // The amount of time elapsed since the last update
+            lastTime = currentTime;
+
+            // If a lot of time has elapsed just treat it as the maximum we allow for in one go
+            if (elapsedTime > MaxElapsedTime)
+            {
+                elapsedTime = MaxElapsedTime;
+            }
+
+            // Add the elapsed time to any 'left over time' from the last update
+            accumulatedTime += elapsedTime;
+
+            var updated = false;
+            // While we have some accumulated time to 'spend' on a tick we keep updating the game and reducing that time
+            while (accumulatedTime >= TargetElapsedTime)
+            {
+                UpdateGame();
+                accumulatedTime -= TargetElapsedTime;
+                updated = true;
+            }
+
+            if (updated)
+            {
+                // If we've updated the gamestate, trigger the form to do a re-draw
+                Invalidate();
+            }
+        }
+
+        public void UpdateGame()
         {
             MakeEnemyBigger();
             MovePlayer();
-            DrawGame();
         }
 
         private void MakeEnemyBigger()
@@ -31,9 +82,8 @@ namespace WinFormsApp1
             Player.Left += PlayerSpeedX;
             Player.Top += PlayerSpeedY;
         }
-        private void DrawGame()
+        private void DrawGame(Graphics graphics)
         {
-            using var graphics = CreateGraphics();
             graphics.Clear(Color.Gray);
             graphics.FillRectangle(new SolidBrush(Player.Color), Player.Left, Player.Top, Player.Width, Player.Height);
             graphics.FillRectangle(new SolidBrush(Enemy.Color), Enemy.Left, Enemy.Top, Enemy.Width, Enemy.Height);
@@ -57,10 +107,18 @@ namespace WinFormsApp1
                 case Keys.Right:
                     PlayerSpeedX = Math.Abs(PlayerSpeedX);
                     break;
+                case Keys.Escape:
+                    Close();
+                    break;
                 default:
                     e.Handled = false;
                     break;
             }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGame(e.Graphics);
         }
     }
 }
